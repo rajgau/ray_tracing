c3d4d78589a4a0538623a38e0929de2286299740
Raj gaurav <singhggaurav14@gmail.com>
2022-10-29 12:43:42 -0700

    solved the issu of dot product

--- ray_tracing.py
+++ ray_tracing.py
@@ -14,14 +14,17 @@ def update(trace=True):
                 pg.quit()
                 return False
     return True
+
 def maper(v,In,Im,On,Om):
     try:val=(((v-In)/(Im-In))*(Om-On))+On
     except:return 0
-    if val<0:val=0
-    elif val>255:val=255
+    if val<On:val=On
+    elif val>Om:val=Om
     return val
 def dis(x1,y1,z1,x2,y2,y3):return M.sqrt(((x2-x1)**2)+((y2-y1)**2)+((z2-z1)**2))
 def dis2(p1,p2):return M.sqrt(((p1[0]-p2[0])**2)+((p1[1]-p2[1])**2)+((p1[2]-p2[2])**2))
+
+Game_light=np.array((20,10,2))
 
 class MainCamera:  
     def __init__(self,screen):
@@ -48,21 +51,19 @@ class MainCamera:
         self.ray_vectors=ray_vectors
     def cast_rays(self,screen):
         for obj in screen.objects:
-            mini = 1000
-            maxi = -1000
             for y in range(self.resolution[1]):
                 for x in range(self.resolution[0]):
                     colition_point = obj.Colision(self.pos,self.ray_vectors[x][y])
                     if colition_point.any():
-                        dist = dis2(self.pos,colition_point)
-                        if dist<mini:mini=dist
-                        if dist>maxi:maxi=dist
-                        c=maper(dist,mini,maxi,0,255)
-                        if c>255:c=255
-                        if c<0:c=0
-                        self.front_buffer[x][y][0]=c
-                        self.front_buffer[x][y][1]=c
-                        self.front_buffer[x][y][2]=c
+                        nv=obj.normal(colition_point)
+                        lv=(Game_light-obj.pos)/dis2(Game_light,obj.pos)
+                        dot = np.dot(nv,lv)            # ^^^^^^
+                        # dot = (np.dot(nv,lv)/2)+0.5    # /\/\/\/\
+                        # dot = max(np.dot(nv,lv),0)       # ^_^_^_
+                        col = abs(dot * obj.col)
+                        self.front_buffer[x][y][0]=col[0]
+                        self.front_buffer[x][y][1]=col[1] 
+                        self.front_buffer[x][y][2]=col[2]
                     
 class Screen:
     def __init__(self,x,y):
@@ -73,29 +74,30 @@ class Screen:
         side_y=height/self.resolution[1]
         for y in range(self.resolution[1]):
             for x in range(self.resolution[0]):
-                pg.draw.rect(window,mainCamera.front_buffer[x][y],(x*side_x,y*side_y,side_x,side_y))
+                try:pg.draw.rect(window,mainCamera.front_buffer[x][y],(x*side_x,y*side_y,side_x,side_y))
+                except:print("Invalid_col_as_input ==>",mainCamera.front_buffer[x][y])
         mainCamera.front_buffer = np.ones(shape=(self.resolution[0],self.resolution[1],3))
 
 class Sphere:
     def __init__(self,x,y,z,r):
         self.pos = np.array((x,y,z))
-        self.col = (255,0,0)
+        self.col = np.array((250,180,155))
         self.r = r
-    def Colision2(self,cam_pos,ray):
+    def Colision(self,cam_pos,ray):
         D=dis2(self.pos,cam_pos)
         camToSelfVec = self.pos-cam_pos
-        camToSelfVecRay = camToSelfVec/D
-        dot_ = np.dot(camToSelfVecRay,ray)
-        theta = M.acos(dot_)
-        ray_mag = D*dot_
-        P = M.sqrt((D**2)-(ray_mag**2))
+        # camToSelfVecRay = camToSelfVec/D
+        dot_ = np.dot(camToSelfVec,ray)
+        # theta = M.acos(dot_)
+        # ray_mag = dot_
+        P = M.sqrt((D**2)-(dot_**2))
         if P<=self.r:
-            return 1
             dv = M.sqrt((self.r**2)-(P**2))
-            ray_hit_mag = ray_mag-dv
-            
-        return 0
-    def Colision(self,cam_pos,ray):
+            ray_hit_mag = (dot_+dv)
+            ray_pos = ray_hit_mag*ray
+            return ray_pos
+        return np.array((0,0,0))
+    def Colision2(self,cam_pos,ray):
         x1,y1,z1 = cam_pos
         x2,y2,z2 = ray
         a,b,c    = self.pos
@@ -109,11 +111,26 @@ class Sphere:
             t = (M.sqrt(disc)-Cl)/(2*Cs)
             return np.array((ray*t)+cam_pos)
         return np.array((0,0,0))
+    def Colision3(self,cam_pos,ray):
+        pointFromT = lambda t:(cam_pos-ray)*t+cam_pos
+        tFromPoint = lambda c:(c/(ray-cam_pos))+cam_pos
+        disOFSurface = lambda p:(dis2(p,self.pos)-self.r)
+        p=None
+        d = disOFSurface(cam_pos)
+        sumer=d
+        while 0.1<d<1000:
+            p = pointFromT(d)
+            d = disOFSurface(p)
+            sumer+=d
+        if d<=100:
+            return np.array(pointFromT(sumer))
+        return np.array((0,0,0))
+    def normal(self,p):return (self.pos-p)/dis2(self.pos,p)
 
-screen=Screen(50,50) #-----1
+screen=Screen(200,200) #-----1
 mainCamera = MainCamera(screen)#------2
 
-screen.objects.append(Sphere(0,0,20,3))#-----3
+screen.objects.append(Sphere(0,0,10,5))#-----3
 
 ###################
 
@@ -123,8 +140,6 @@ screen.set_pixels(mainCamera)#-------5
 a=0
 while update():
     # m=maper(M.sin(a),-1,1,0.1,4)
-    # mainCamera.cast_rays(screen)#-------4
-    # screen.set_pixels(mainCamera)#-------5
     a+=0.01
     # print(m)
     
